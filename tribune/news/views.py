58014f464e9de2348a0c4b5a0b8aed3d62ffeea5
MrazTevin <millatevin@gmail.com>
2018-02-19 16:18:39 +0300

    past days view function

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -34,4 +34,24 @@ def convert_dates(dates):
 
     # returning actual day of the week
     day = days[day_number]
+
     return day
+
+def past_days_news(request,past_date):
+        # convert data from string Url
+
+    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    
+    day = convert_dates(date)
+    html = f'''
+      <html>
+        <body>
+          <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+      </html>
+         '''
+    
+    return HttpResponse(html)
+    
+
+
+
