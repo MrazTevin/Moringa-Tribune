34c68c7412520909900414c07eb6e77338071a5b
MrazTevin <millatevin@gmail.com>
2018-02-20 16:55:00 +0300

    articletest class

--- tribune/news/tests.py
+++ tribune/news/tests.py
@@ -33,8 +33,32 @@ class EditorTestClass(TestCase):
     #     self.james.update_editor()
     #     last_name = "Ulemsee"
     #     self.assertTrue(self.james.update_editor(), last_name)
+
+
+class ArticleTestClass(TestCase):
         
 
+        def setUp(self):
+            # creating a new editor and saving it
+
+            self.james= Editor(first_name='James',last_name='Muriuki',email=
+            'james@moringaschool.com')
+
+            self.james.save_editor()
+
+            # creating a new tag and saving it
+            self.new_tag = tags(name ='testing')
+            self.new_tag.save()
+
+            self.new_article= Article(title='TestArticle'),post='This is a random test Post',editor=self.james)
+            self.new_article.save()
+
+            self.new_article.tags.add(self.new_tag)
         
+        def tearDown(self):
+            Editor.objects.all().delete()
+            tags.objects.all.delete()
+            Article.objects.all().delete()
+            
 
  
